02ad61b10d85931ca19d8a974d722c1db992ff58
ihateb335 <zhar.misha@gmail.com>
2026-05-15 17:23:11 +0300

    Add address book

---
+++ src/address_book.py
@@ -0,0 +1,43 @@
+from src.record import Record
+
+
+class AddressBook:
+    def __init__(self):
+        """
+        Initialize the address book.
+        """
+        self.data = {}
+
+    def add_record(self, record: Record):
+        """
+        Add a record to the address book.
+
+        Args:
+            record (Record): The record to add.
+        """
+        self.data[record.name.value] = record
+
+    def find_record(self, name: str) -> Record | None:
+        """
+        Find a record in the address book.
+
+        Args:
+            name (str): The name of the record to find.
+
+        Returns:
+            Record | None: The record if found, None otherwise.
+        """
+        return self.data.get(name)
+
+    def remove_record(self, name: str) -> bool:
+        """
+        Remove a record from the address book.
+
+        Args:
+            name (str): The name of the record to remove.
+
+        Returns:
+            bool: True if the record was removed, False otherwise.
+        """
+
+        return self.data.pop(name, None) is not None

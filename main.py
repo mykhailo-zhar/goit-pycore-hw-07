220efe59c65ab9922ae1c16116995c8b3c9e9884
ihateb335 <zhar.misha@gmail.com>
2026-05-19 20:40:57 +0300

    Add contacts_bot main to main.py

--- main.py
+++ main.py
@@ -1,41 +1,4 @@
-from src.address_book import AddressBook
-from src.record import Record
-
-
-def main():
-    # Створення нової адресної книги
-    book = AddressBook()
-
-    # Створення запису для John
-    john_record = Record("John")
-    john_record.add_phone("1234567890")
-    john_record.add_phone("5555555555")
-
-    # Додавання запису John до адресної книги
-    book.add_record(john_record)
-
-    # Створення та додавання нового запису для Jane
-    jane_record = Record("Jane")
-    jane_record.add_phone("9876543210")
-    book.add_record(jane_record)
-
-    # Виведення всіх записів у книзі
-    for name, record in book.data.items():
-        print(record)
-
-    # Знаходження та редагування телефону для John
-    john = book.find_record("John")
-    john.edit_phone("1234567890", "1112223333")
-
-    print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
-
-    # Пошук конкретного телефону в записі John
-    found_phone = john.find_phone("5555555555")
-    print(f"{john.name.value}: {found_phone.value}")  # Виведення: 5555555555
-
-    # Видалення запису Jane
-    book.remove_record("Jane")
-
+from src.scripts.contacts_bot import main
 
 if __name__ == "__main__":
     main()
